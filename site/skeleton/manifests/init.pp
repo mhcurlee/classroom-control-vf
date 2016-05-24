feab2390514ea21e5d679cd9fae5b2b038cdaa7f
Marvin Curlee <marvin.curlee@gmail.com>
2016-05-24 12:13:39 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,20 @@
-dsd
+class  skeleton {
+
+file {"/etc/skel":
+  ensure => directory,
+  owner   => '0',
+  group   => '0',
+  }
+  
+  
+file {"etc/skel/.bashrc":
+  ensure => file,
+  source => 'puppet:///modules/skeleton/bashrc',
+  owner  => '0',
+  group  =>  '0',
+  require =>  File['/etc/skel'],
+}
+
+
+
+}
