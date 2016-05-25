50763940e6165da3072cc65099313f5ed4d2d946
Marvin Curlee <marvin.curlee@gmail.com>
2016-05-25 07:20:02 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,8 +1,34 @@
 class nginx {
 
+package {'nginx': 
+  ensure   => present,
+}
+
+file {'/etc/nginx/nginx.conf':
+  ensure   => 'present',
+  source   => 'puppet:///modules/nginx/nginx.conf',
+  owner    => '0',
+  group    => '0',
+  
+  }
+
+file {'/etc/nginx/conf.d/default.conf':
+   ensure    => 'present',
+   source    => 'puppet:///modules/nginx/default.conf',
+   owner     => '0',
+   group     => '0',
+   }
 
 
 
+
+service {'nginx':
+   ensure  => 'running',
+   enable  => 'true',
+   subscribe => [ File['/etc/nginx/conf.d/default.conf'], File['/etc/nginx/nginx.conf'] ],
+}
+  
+  
 
 
 
