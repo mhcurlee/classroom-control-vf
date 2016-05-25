45624ddbd5aad7eaf26c101024642e97cda74eb1
Marvin Curlee <marvin.curlee@gmail.com>
2016-05-25 10:52:10 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,7 +1,18 @@
-class users::admins { users::managed_user { 'joe': } users::managed_user { 'alice':
+class users::admins { 
+
+
+users::managed_user { 'joe': } 
+
+users::managed_user { 'alice':
 group => 'staff', }
-users::managed_user { 'aaron': group => 'staff',
+
+
+users::managed_user { 'aaron':
+group => 'staff',
 }
+
+
 group { 'staff':
 ensure => present, }
+
 }
