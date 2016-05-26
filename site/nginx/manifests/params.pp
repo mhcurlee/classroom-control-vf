4a247a6756066b2222d3ff2e55fd97cf59d056fe
Marvin Curlee <marvin.curlee@gmail.com>
2016-05-26 12:05:37 -0500

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,1 +1,29 @@
+class nginx::params{
 
+case $::osfamily {
+'redhat','debian' : { 
+
+$package = 'nginx' 
+$owner = 'root'
+$group = 'root' 
+$docroot = $root 
+$confdir = '/etc/nginx' 
+$logdir = '/var/log/nginx'
+
+}
+
+
+default : {
+fail("Module ${module_name} is not supported on ${::osfamily}") }
+
+}
+
+# user the service will run as. Used in the nginx.conf.erb template
+$user = $::osfamily ? { 
+'redhat' => 'nginx', 
+'debian' => 'www-data', 
+}
+
+
+
+}
